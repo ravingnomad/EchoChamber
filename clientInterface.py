1ec054a5efc23ebfb2042950c2415de6d29b441c
ravingnomad <joshbervera@yahoo.com>
2021-09-16 14:52:02 -0700

    Moved all command format checking from echoChamberClient to clientInterface
    clientInterface handles prompts for user input and storing user input for the client side of the program, so makes sense for the command format checking to be in clientInterface instead of echoChamberClient

--- clientInterface.py
+++ clientInterface.py
@@ -6,8 +6,12 @@ class clientInterface():
         self.serverAddress = ""
         self.userRecentCommand = ""
         self.validCommands = ['send', 'sendSMS', 'SMSLog', 'help', 'ls', 'q']
+
         
-    
+    def promptServerAddress(self) -> None:
+        self.serverAddress = input("Enter server ip address \n ->")
+        
+        
     def getServerAddress(self) -> str:
         return self.serverAddress
         
@@ -31,52 +35,20 @@ class clientInterface():
         
         
     def _commandCorrectlyFormatted(self, userInput: str) -> bool:
-        splitUserInput = userInput.split(' ')
-        command = splitUserInput[0]
-        if command not in self.commands:
+        command, *args = userInput.split(' ')
+        noFormatError = True
+        if command == "send":
+            noFormatError = len(args) in (1, 2)
+        elif command == "sendSMS":
+            noFormatError = len(args) == 2
+        elif command in ("ls", "SMSLog", "help", "q"):
+            noFormatError = len(args) == 0
+        else:
             self._printCommandNotExistError(command)
             return False
-
-        if command == "send" and len(splitUserInput) not in (2, 3):
+        if noFormatError == False:
             self._printIncorrectlyFormattedError(command)
-            return False
-#             elif self._fileExists(command[1]) == False:
-#                 print("ERROR: The file '{}' does not exist. Type 'ls' to see list of available files in directory.\n".format(command[1]))
-#                 return True
-            
-        elif command == "sendSMS" and len(splitUserInput) != 3:
-            self._printIncorrectlyFormattedError(command)
-            return False
-#             elif self._fileExists(command[-1]) == False:
-#                 print("ERROR: The file '{}' does not exist. Type 'ls' to see list of available files in directory.\n".format(command[1]))
-#                 return True
-            
-#             elif self._fileCorrectType(command[-1]) == False:
-#                 extension = command[-1].split('.')[-1]
-#                 print("ERROR: Cannot send files of type '{}'. Type 'help' to see supported file types.\n".format(extension))
-#                 return True
-
-        elif command == "ls" and len(splitUserInput) != 1:
-            self._printIncorrectlyFormattedError(command)
-            return False
-#             if len(command) > 2 or (len(command) == 2 and command[1] != '-s'):
-#                 print("ERROR: 'ls' command incorrectly formatted. Type 'help' to see correct format.\n")
-#                 return True
-                
-                
-        elif command == "SMSLog" and len(splitUserInput) != 1:
-            self._printIncorrectlyFormattedError(command)
-            return False
-#             if len(command) > 1:
-#                 print("ERROR: 'SMSLog' command incorrectly formatted. Type 'help' to see correct format.\n")
-#                 return True
-            
-        elif command == "help" and len(splitUserInput) != 1:
-            self._printIncorrectlyFormattedError(command)
-            return False
-#             print("ERROR: 'help' command incorrectly formatted. Type 'help' to see correct format.\n")
-#             return False
-        return True
+        return noFormatError
     
     
     def _printCommandNotExistError(self, command: str) -> None:
@@ -85,11 +57,3 @@ class clientInterface():
     
     def _printIncorrectlyFormattedError(self, command: str) -> None:
         print(f"ERROR: '{command}' incorrectly formatted. Type 'help' to see correct format \n")
-    
-    
-    def promptServerAddress(self) -> None:
-        self.serverAddress = input("Enter server ip address \n ->")
-    
-    
-    def promptClientCommand(self) -> None:
-        None
--- echoChamberClient.py
+++ echoChamberClient.py
@@ -35,92 +35,43 @@ class echoChamberClient(Connection):
     
     def _processCommands(self, userInput: str) -> None:
         command, *args = userInput.split(' ')
-        if self._commandHasErrors(command) == False:
-            if command == 'q':
-                self._sendData('q')
-                self.abort = True
-            elif command == "sendSMS":
-                self._sendData(command)
-                checkError = self._recvData().decode()
-                if checkError == "Recipient Found":
-                    fileName = check[-1]
-                    self._sendFile(fileName)
-                    serverMsg = self._recvData().decode()
-                    print(serverMsg + '\n')
-                else:
-                    print(checkError + '\n')
-            
-            elif command == "send":
-                fileName = check[1]
-                self._sendData(command)
+        if command == 'q':
+            self._sendData('q')
+            self.abort = True
+        elif command == "sendSMS":
+            self._sendData(command)
+            checkError = self._recvData().decode()
+            if checkError == "Recipient Found":
+                fileName = check[-1]
                 self._sendFile(fileName)
+                serverMsg = self._recvData().decode()
+                print(serverMsg + '\n')
+            else:
+                print(checkError + '\n')
+        
+        elif command == "send":
+            fileName = check[1]
+            self._sendData(command)
+            self._sendFile(fileName)
 
-            elif command == "SMSLog":
-                '''Get message from server detailing the logged sms info'''
-                self._sendData(command)
-                self._requestSMSLog()
-                
-            elif command == "ls":
-                self._displayFiles()
-
-            elif command == "help":
-                print("Commands:\n")
-                print("\t\"send\" [file name with extension] [optional: new file name]:  makes a copy of specified file and sends it to receiving server. Will save file on server-side with new name if specified.\n")
-                print("\t\"sendSMS\" [name of recipient] [file name with extension]: sends a file and recipient name to the server so that file can be converted to email and texted to recipient's phone. Limited to text, PNG, IMG, GIF, and WEBM files.\n")
-                print("\t\"SMSLog\": receive information from server describing what phone contacts are available for use along with a small description of who\what they are\n")
-                print("\t\"ls\" : lists the files that are currently in the directory.\n\n")
-                print("\t\"q\": terminates connection with server-side. In current condition, also shuts down server.\n")        
-
-    def _commandHasErrors(self, command) -> bool:
-        """Takes a command, split by its white spaces, and sees if it is formatted correctly. If it is,
-        return 'False' as in there are no errors. Otherwise, return 'True'
-        as in there is an error."""
-        if command[0] not in self.commands:
-            print("ERROR: Command '{}' does not exist. Type 'help' to see available commands.\n".format(command[0]))
-            return True
-
-        check = command[0]
-        if check == "send":
-            if len(command) < 2 or len(command) > 3:
-                print("ERROR: 'send' command incorrectly formatted. Type 'help' to see correct format.\n")
-                return True
-            elif self._fileExists(command[1]) == False:
-                print("ERROR: The file '{}' does not exist. Type 'ls' to see list of available files in directory.\n".format(command[1]))
-                return True
+        elif command == "SMSLog":
+            '''Get message from server detailing the logged sms info'''
+            self._sendData(command)
+            self._requestSMSLog()
             
-        elif check == "sendSMS":
-            if len(command) < 3 or len(command) > 3:
-                print("ERROR: 'sendSMS' command incorrectly formatted. Type 'help' to see correct format.\n")
-                return True
-            
-            elif self._fileExists(command[-1]) == False:
-                print("ERROR: The file '{}' does not exist. Type 'ls' to see list of available files in directory.\n".format(command[1]))
-                return True
-            
-            elif self._fileCorrectType(command[-1]) == False:
-                extension = command[-1].split('.')[-1]
-                print("ERROR: Cannot send files of type '{}'. Type 'help' to see supported file types.\n".format(extension))
-                return True
+        elif command == "ls":
+            self._displayFiles()
 
-        elif check == "ls":
-            if len(command) > 2 or (len(command) == 2 and command[1] != '-s'):
-                print("ERROR: 'ls' command incorrectly formatted. Type 'help' to see correct format.\n")
-                return True
-                
-        elif check == "SMSLog":
-            if len(command) > 1:
-                print("ERROR: 'SMSLog' command incorrectly formatted. Type 'help' to see correct format.\n")
-                return True
-            
-        elif check == "help" and len(command) > 1:
-            print("ERROR: 'help' command incorrectly formatted. Type 'help' to see correct format.\n")
-            return True
+        elif command == "help":
+            print("Commands:\n")
+            print("\t\"send\" [file name with extension] [optional: new file name]:  makes a copy of specified file and sends it to receiving server. Will save file on server-side with new name if specified.\n")
+            print("\t\"sendSMS\" [name of recipient] [file name with extension]: sends a file and recipient name to the server so that file can be converted to email and texted to recipient's phone. Limited to text, PNG, IMG, GIF, and WEBM files.\n")
+            print("\t\"SMSLog\": receive information from server describing what phone contacts are available for use along with a small description of who\what they are\n")
+            print("\t\"ls\" : lists the files that are currently in the directory.\n\n")
+            print("\t\"q\": terminates connection with server-side. In current condition, also shuts down server.\n")        
 
-        elif check == "q" and len(command) > 1:
-            print("ERROR: 'q' command incorrectly formatted. Type 'help' to see correct format.\n")
-            return True
-
-        return False
+    
+    
     
     def _requestSMSLog(self):
         """Requests a text log from the server that details the phone/contact info
@@ -132,6 +83,7 @@ class echoChamberClient(Connection):
             if line != "END":
                 print(line)
         print('\n')
+
 
     def _displayFiles(self):
         for file in os.listdir():
