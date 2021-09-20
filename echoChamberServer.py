6ec0f8d837c4a9fd85d412fc0ec126dcedb9691c
ravingnomad <joshbervera@yahoo.com>
2021-09-20 13:18:07 -0700

    Changed some variable names in textFile() to be more descriptive

--- echoChamberServer.py
+++ echoChamberServer.py
@@ -40,22 +40,13 @@ class echoChamberServer(Connection):
         print("Closed connection with client\n")
                     
 
-    def _waitForCommands(self):
+    def _waitForCommands(self) -> None:
         userInput = self._recvStrData()
         command, *args = userInput.split(" ")
         if command == "send":
             self._sendFileToClient(args[0])
         elif command == "sendSMS":
             self._sendSMSFile(args)
-            # recipient = args[0]
-            # fileName = args[1]
-            # if self._smsRecipientValid(recipient) == True:
-            #     noErrors = self._recvData().decode()
-            #     if (noErrors == "File Exists"):
-            #         self._copyFile(fileName)
-            #         self._textFile(recipient, fileName)
-            #     elif (noErrors == "Error found"):
-            #         print("\nClient found error(s) when trying to open file. Stopping command execution\n")
         elif command == "SMSLog":
             self._sendSMSInfo()
         elif command == "ls":
@@ -96,7 +87,7 @@ class echoChamberServer(Connection):
             self._sendData("File not found") 
             
     
-    def _fileExists(self, fileName) -> bool:
+    def _fileExists(self, fileName: str) -> bool:
         return fileName in os.listdir()
         
         
@@ -104,7 +95,7 @@ class echoChamberServer(Connection):
         return recipientName in self.validUsers.keys()
             
             
-    def _sendSMSInfo(self):
+    def _sendSMSInfo(self) -> None:
         log = ""
         for user in self.validUsers.keys():
             log += user
@@ -112,47 +103,42 @@ class echoChamberServer(Connection):
         self._sendData(log)
 
 
-    def _textFile(self, recipient, fileName):
+    def _textFile(self, recipient: str, fileName: str) -> None:
         """Given a valid recipient name, server will send a text message to the recipient that
         contains the specified file. Since current SMS limits sent file sizes to be around 1.8MB,
         will check the size of the file and if it is greater than 1.8MB, will attempt to compress
         using FFmpeg. CURRENT COMPRESSION ONLY SUPPORTS MP4 AND WEBM. If file is still too large,
-        will attempt to send the file, but will send client a message stating that file is too big
-        and likely it will not be able to be sent. There are no guarantees that delivery will be
+        will still attempt to send the file. There are no guarantees that delivery will be
         successful as entirely dependent on rules imposed by client's mobile plan. After file is sent,
         if it was compressed, the compressed file will be deleted. If compression used, must delete
         resulting compressed file after reading its info as errors will arise if another compression
         occurs and saves with the same file name as the compressed file."""
 
-        file = None
+        fileData = None
         compressed = False
-        extension = fileName.split('.')[-1]
-        clientMsg = 'Texting file to recipient'
-
+        fileExtension = fileName.split('.')[-1].lower()
+        
         if self._fileTooBig(fileName):
-            if extension.lower() in ('webm', 'mp4'):
-                clientMsg = self._compress(fileName, extension)
+            if fileExtension in ('webm', 'mp4'):
+                self._compress(fileName, fileExtension)
                 compressed = True
-            else:
-                clientMsg = 'WARNING: File larger than 1.8MB and does not support compression. Highly probable that file will not reach recipient.'
-        #self._sendData(clientMsg)
         
         if compressed == True:
-            with open('output.{}'.format(extension), 'rb') as file:
-                file = file.read()
-            os.remove('output.{}'.format(extension))
+            with open(f'output.{fileExtension}', 'rb') as compressedFile:
+                fileData = compressedFile.read()
+            os.remove(f'output.{fileExtension}')
 
-        elif file == None and extension.lower() != 'txt':
-            with open(fileName, 'rb') as file:
-                file = file.read()
+        elif fileData == None and fileExtension != 'txt':
+            with open(fileName, 'rb') as nonTextFile:
+                fileData = nonTextFile.read()
         else:
-            with open(fileName, 'r', encoding = 'latin-1') as file:
-                file = file.read()
+            with open(fileName, 'r', encoding = 'latin-1') as textFile:
+                fileData = textFile.read()
                 
         msg = MIMEMultipart()
-        if extension.lower() in ('webm', 'mp4'):
+        if fileExtension in ('webm', 'mp4'):
             data = MIMEBase('video', 'mp4')
-            data.set_payload(file)
+            data.set_payload(fileData)
             encoders.encode_base64(data)
             data.add_header('Content-Disposition', 'attachment', filename = fileName)
             msg.attach(data)
@@ -162,8 +148,8 @@ class echoChamberServer(Connection):
             text = MIMEText("test")
             msg.attach(text)
 
-        elif extension.lower() == 'txt':
-            text = MIMEText(file, _charset = "UTF-8")
+        elif fileExtension == 'txt':
+            text = MIMEText(fileData, _charset = "UTF-8")
             msg.attach(text)
 
         else:
@@ -172,7 +158,7 @@ class echoChamberServer(Connection):
             msg['To'] = 'us'
             text = MIMEText("test")
             msg.attach(text)
-            image = MIMEImage(file)
+            image = MIMEImage(fileData)
             msg.attach(image)
             
             
@@ -182,15 +168,12 @@ class echoChamberServer(Connection):
         server.sendmail('computer', self.validUsers[recipient]['SMS'], msg.as_string())
         server.close()
         
-
-
-    def _compress(self, fileName, extension):
-        """Given a filename, will compress the file using ffmpeg in order for it to be deliverable
-        to a recipient's cellphone. If the compressed file is still >= 1.8MB, will return an error
-        statement stating so. For now, can only compress webm and mp4 files."""
+        
+    #attempts to compress files > 1.8MB; sizes above this are tricky to send; CURRENT COMPRESSION ONLY SUPPORTS MP4 AND WEBM
+    def _compress(self, fileName: str, extension: str) -> None:
         encoder = None
         crf = 0
-        output = 'output.' + extension
+        outputFileName = 'output.' + extension
         result = "File compressed. Sending to recipient."
         if extension.lower() == 'mp4':
             crf = 30
@@ -198,19 +181,14 @@ class echoChamberServer(Connection):
         elif extension.lower() == 'webm':
             crf = 63
             encoder = 'libvpx-vp9'
-   
         inD = {fileName: None}
-        outD = {output: '-c:v {} -crf {}'.format(encoder, crf)}
+        outD = {outputFileName: '-c:v {} -crf {}'.format(encoder, crf)}
         ff = ffmpy.FFmpeg(executable = 'C:\\ffmpeg\\bin\\ffmpeg.exe', inputs = inD, outputs = outD)
         print("Running FFmpeg command: " + ff.cmd)
         ff.run()
 
-        if self._fileTooBig(output):
-            result = 'WARNING: File larger than 1.8MB. Highly probable that file will not reach recipient.'
-        return result
 
-
-    def _copyFile(self, fileName):
+    def _copyFile(self, fileName: str) -> None:
         with open(fileName, 'wb') as file:
             self._sendData("Ready to receive file.")
             data = self._recvData()
@@ -219,74 +197,16 @@ class echoChamberServer(Connection):
         print("Finished copying file\n")
 
 
-    def _fileTooBig(self, fileName):
+    def _fileTooBig(self, fileName: str) -> None:
         fileStats = os.stat(fileName)
         size = fileStats.st_size
         return size >= 1800000
 
 
-    def _sendFileInfo(self):
+    def _sendFileInfo(self) -> None:
         fileInfo = ""
         for fileName in os.listdir():
             statinfo = os.stat(fileName)
             size = statinfo.st_size / 1000000
             fileInfo += fileName + '\t' + str(round((size), 2)) + " MB\n"
         self._sendData(fileInfo)
-
-
-
-
-
-
-
-                
-# def _commandHasErrors(self, command) -> bool:
-#         """Takes a command, split by its white spaces, and sees if it is formatted correctly. If it is,
-#         return 'False' as in there are no errors. Otherwise, return 'True'
-#         as in there is an error."""
-#         if command[0] not in self.commands:
-#             print("ERROR: Command '{}' does not exist. Type 'help' to see available commands.\n".format(command[0]))
-#             return True
-#
-#         check = command[0]
-#         if check == "send":
-#             if len(command) < 2 or len(command) > 3:
-#                 print("ERROR: 'send' command incorrectly formatted. Type 'help' to see correct format.\n")
-#                 return True
-#             elif self._fileExists(command[1]) == False:
-#                 print("ERROR: The file '{}' does not exist. Type 'ls' to see list of available files in directory.\n".format(command[1]))
-#                 return True
-#
-#         elif check == "sendSMS":
-#             if len(command) < 3 or len(command) > 3:
-#                 print("ERROR: 'sendSMS' command incorrectly formatted. Type 'help' to see correct format.\n")
-#                 return True
-#
-#             elif self._fileExists(command[-1]) == False:
-#                 print("ERROR: The file '{}' does not exist. Type 'ls' to see list of available files in directory.\n".format(command[1]))
-#                 return True
-#
-#             elif self._fileCorrectType(command[-1]) == False:
-#                 extension = command[-1].split('.')[-1]
-#                 print("ERROR: Cannot send files of type '{}'. Type 'help' to see supported file types.\n".format(extension))
-#                 return True
-#
-#         elif check == "ls":
-#             if len(command) > 2 or (len(command) == 2 and command[1] != '-s'):
-#                 print("ERROR: 'ls' command incorrectly formatted. Type 'help' to see correct format.\n")
-#                 return True
-#
-#         elif check == "SMSLog":
-#             if len(command) > 1:
-#                 print("ERROR: 'SMSLog' command incorrectly formatted. Type 'help' to see correct format.\n")
-#                 return True
-#
-#         elif check == "help" and len(command) > 1:
-#             print("ERROR: 'help' command incorrectly formatted. Type 'help' to see correct format.\n")
-#             return True
-#
-#         elif check == "q" and len(command) > 1:
-#             print("ERROR: 'q' command incorrectly formatted. Type 'help' to see correct format.\n")
-#             return True
-#
-#         return False
