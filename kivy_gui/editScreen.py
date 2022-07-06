00a8e44e4f655e8072b6cc00d024746c83d23532
ravingnomad <joshbervera@yahoo.com>
2022-07-06 13:00:52 -0700

    Implemented basic layout of edit screen

--- kivy_gui/editScreen.py
+++ kivy_gui/editScreen.py
@@ -3,13 +3,18 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder 
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
 
 Builder.load_file("edit_screen.kv")
 
 class EditScreenLayout(BoxLayout):
+    preset_name_input = ObjectProperty(None)
+    
     def __init__(self, **kwargs):
         super(EditScreenLayout, self).__init__(**kwargs)
         
+        print(self.preset_name_input.text_input.text)
+    
 
 class mainApp(App):
     def build(self):
