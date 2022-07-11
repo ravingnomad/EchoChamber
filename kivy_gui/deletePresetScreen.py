886aca4b84bffd156bdc43424884e0ff9830fb3a
ravingnomad <joshbervera@yahoo.com>
2022-07-11 10:00:36 -0700

    Created basic layout of the delete preset screen.

---
+++ kivy_gui/deletePresetScreen.py
@@ -0,0 +1,28 @@
+from kivy.config import Config
+Config.set('graphics', 'resizable', 0)
+
+from kivy.core.window import Window
+windowMinWidth = 500
+windowMinHeight = 180
+Window.minimum_width, Window.minimum_height = windowMinWidth, windowMinHeight
+Window.size = (windowMinWidth, windowMinHeight)
+
+from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+
+
+Builder.load_file("delete_preset_screen.kv")
+
+class DeletePresetScreen(FloatLayout):
+    pass 
+
+
+class MyApp(App):
+    def build(self):
+        return DeletePresetScreen()
+    
+    
+    
+if __name__ == "__main__":
+    MyApp().run()
